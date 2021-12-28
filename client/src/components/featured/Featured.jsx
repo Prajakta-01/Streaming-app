d9c91b3ac6687dad613e72e6b18c4492918243fc
Prajakta-01 <96041611+Prajakta-01@users.noreply.github.com>
2021-12-28 16:34:28 +0530

    Update Featured.jsx
    Access token updated

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -12,7 +12,7 @@ export default function Featured({ type }) {
         const res = await axios.get(`/movies/random?type=${type}`, {
           headers: {
             token:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYzFlNjc1YWU2MWVjM2UxMDZjZDI4MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MDA5OTE4OSwiZXhwIjoxNjQwNTMxMTg5fQ.NuACvA1QA_ZwgxnANBmFAJ3KZPIZDPWHrkX3mQwQbSU",
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYzFlNjc1YWU2MWVjM2UxMDZjZDI4MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MDY2NzY1MCwiZXhwIjoxNjQxMDk5NjUwfQ.q4Bts2g7lxkEOnONsB6Vb676jey-auklDJ_phWqgsT8",
           },
         });
         setContent(res.data[0]);
